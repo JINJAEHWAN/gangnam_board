37424c26468875bfd8277139c923b7289402c6fb
JPR <puruem1215@naver.com>
2025-05-12 12:00:17 +0900

    boardDAO listaction 추가

--- src/main/java/Board/BoardDAO.java
+++ src/main/java/Board/BoardDAO.java
@@ -3,10 +3,14 @@ package Board;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import board.BoardVO;
 
 public class BoardDAO {
 	
@@ -25,7 +29,29 @@ public class BoardDAO {
 		}
 	}
 	
+	public List<BoardVO> boardList() {
+		List<BoardVO> list = new ArrayList<>();
+		
+		return list;
+	}
 	
+	public List<BoardVO> searchByKeyword(String keyword) {
+		List<BoardVO> list = new ArrayList<>();
+		
+		return list;
+	}
+	
+	public List<BoardVO> searchByCategory(String category) {
+		List<BoardVO> list = new ArrayList<>();
+		
+		return list;
+	}
+	
+	public List<BoardVO> searchByKeywordAndCategory(String keyword, String category) {
+		List<BoardVO> list = new ArrayList<>();
+		
+		return list;
+	}
 	
 	
 	
