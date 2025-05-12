9e7b4c32ea8402eed5e6c3a33d4e02c9d0449d46
JPR <puruem1215@naver.com>
2025-05-12 18:00:50 +0900

    Update: 로그인 과정 수정 & 주석 추가

--- src/main/java/member/loginProcessAction.java
+++ src/main/java/member/loginProcessAction.java
@@ -2,6 +2,7 @@ package member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import servletController.*;
 
@@ -11,20 +12,42 @@ public class loginProcessAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ActionForward forward = new ActionForward();
-		// TODO Auto-generated method stub
 		MemberDAO memDAO  = new MemberDAO();
 		MemberVO memVO1 =new MemberVO();
+		
+		/****************************
+		 * 로그인, login Form에서 request 처리 과정
+		 ***************************/
 		memVO1.setUserId(request.getParameter("userId"));
 		memVO1.setPassword(request.getParameter("password"));
+		
+		
+		/****************************
+		 * 로그인, DAO 유효성 검사 과정
+		 ***************************/
 		MemberVO memVO2 = memDAO.login(memVO1.getUserId());
 		System.out.println("dag");
 		forward.setPath("login.me");
 		
-		if(memVO2!=null) {
+		
+		/****************************
+		 * 로그인, 실패 프로세스
+		 ***************************/
+		if(memVO2!=null && memVO2.getPassword().equals(memVO1.getPassword())) {
 			forward.setPath("list.co");
 			forward.setRedirect(true);
 			System.out.println("로그인 o");
 		}
+
+		
+		/****************************
+		 * 로그인, 성공 프로세스
+		 ****************************/
+		
+		//세션 등록 과정
+		HttpSession session = request.getSession();
+		session.setAttribute("userId", request.getParameter("userId"));
+		
 		
 		return forward;
 	}
