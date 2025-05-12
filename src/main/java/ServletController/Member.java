633b1e865134dea34131d52f60bd07354d547977
jjh <jinjh99@gmail.com>
2025-05-12 10:30:37 +0900

    임시

---
+++ src/main/java/ServletController/Member.java
@@ -0,0 +1,62 @@
+package ServletController;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.BoardListAction;
+import controller.Action;
+import controller.ActionForward;
+import controller.String;
+
+/**
+ * Servlet implementation class Member
+ */
+@WebServlet("/Member")
+public class Member extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Member() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	public void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+	
+		String url = request.getRequestURI();
+		String path = request.getContextPath();
+		String command = url.substring(path.length());
+		System.out.println("command: "+command);
+		
+		Action action = null;
+		ActionForward forward = null;
+		
+		if(command.equals("/login.me")) {
+			action = new BoardListAction();
+		}
+	}
+
+}
