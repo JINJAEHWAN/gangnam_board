0b244e5560ea225df917897b7f045886519f6a0a
JPR <puruem1215@naver.com>
2025-05-12 11:42:17 +0900

    boardlistaction 추가

--- src/main/java/Board/BoardListAction.java
+++ src/main/java/Board/BoardListAction.java
@@ -1,5 +1,7 @@
 package Board;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,7 +13,36 @@ public class BoardListAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");		
+		
+		BoardDAO dao = new BoardDAO();
+		List<BoardVO> list = null;
+		
+		String keyword = request.getParameter("kewword");
+		String category = request.getParameter("category");
+		
+		if (keyword != null && !keyword.trim().isEmpty() && category != null && !category.trim().isEmpty()) {
+			list = dao.searchByKeywordAndCategory(keyword, category);
+		}
+		else if (keyword != null && !keyword.trim().isEmpty()) {
+			list = dao.searchByKeyword(keyword);
+		}
+		else if (category != null && !category.trim().isEmpty()) {
+			list = dao.searchByCategory(category);
+		}
+		else {
+			list = dao.boardList();
+		}
+		
+		request.setAttribute("list", list);
+		
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("/boardList.jsp");
+		
+		return forward;
 	}
 	
 	
