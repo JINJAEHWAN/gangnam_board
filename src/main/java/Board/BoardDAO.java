d832d5e6a5fef5b73a06508463857bb7a4e41cf6
KJS <wnsjtq781@naver.com>
2025-05-12 12:30:50 +0900

    추가: BoardWriteAction.java, 수정: BoardDAO.java

--- src/main/java/Board/BoardDAO.java
+++ src/main/java/Board/BoardDAO.java
@@ -8,6 +8,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+
 public class BoardDAO {
 	
 	private Connection conn;
@@ -29,6 +30,31 @@ public class BoardDAO {
 		try { if(rs != null) rs.close(); } catch(Exception e) {}
 		try { if(pstmt != null) pstmt.close(); } catch(Exception e) {}
 		try { if(conn != null) conn.close(); } catch(Exception e) {}
+	}
+	
+	
+	//BoardWriteAction
+	public int boardInsert(BoardVO vo) {
+		int result = 0;
+		String sql = "INSERT INTO TRAVEL_BOARD (BOARD_TITLE, BOARD_CONTENT, BOARD_CATEGORY, TRAVEL_LOCATION, TRAVEL_PERIOD, TRAVEL_BUDGET, INST_USER, UPDT_USER) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getBoardTitle());
+			pstmt.setString(2, vo.getBoardContent());
+			pstmt.setString(3, vo.getBoardCategory());
+			pstmt.setString(4, vo.getTravelLocation());
+			pstmt.setString(5, vo.getTravelPeriod());
+			pstmt.setInt(6, vo.getTravelBudget());
+			pstmt.setString(7, vo.getInstUser());
+			pstmt.setString(8, vo.getInstUser());
+			result = pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeCon();
+		}
+		return result;
 	}
 
 }
